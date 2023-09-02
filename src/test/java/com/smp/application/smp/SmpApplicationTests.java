14867e7c8cbecc2d81d45bbadb091467f46b32ed
md.adnan77 <md.adnan77@gmail.com>
2023-09-02 21:10:10 +0800

    first commit

---
+++ src/test/java/com/smp/application/smp/SmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smp.application.smp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
